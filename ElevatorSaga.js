8ff7fe9b221de163799691f46cda44e7807505c2
Sam Sadtler <sam.sadtler@gmail.com>
2015-04-22 11:09:54 -0400

    changed the number of Lvls to 5+1. Added comments

--- ElevatorSaga.js
+++ ElevatorSaga.js
@@ -1,23 +1,23 @@
 
-/////Funtional Code up to level 7
+
 {
     init: function(elevators, floors) {
-         // Let's use the first elevator
-        // var elevator = elevators[0];
+        //code works through lvl 5 and 6 with tweak
         var floor = floors[floors.length];
        
         var elevatorCall = function(elevatorNumber){   
             var elevator = elevatorNumber;
+            //comment out elevator.on("idle") to make it past lvl 6
             elevator.on("idle", function() { toFloor(0); });
             elevator.on("floor_button_pressed", function(floorNum) { 
                 for (i = 0 ; i < elevator.getPressedFloors().length; i++ ){
                     toFloor(floorNum);
                 }
             });   
-        var toFloor = function(floorNumber){
-                elevator.goToFloor(floorNumber);
-                
-        };
+            var toFloor = function(floorNumber){
+                    elevator.goToFloor(floorNumber);
+                    
+            };
 
         }
         for (var e = 0 ; e < elevators.length; e++){
